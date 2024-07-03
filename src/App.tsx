999700e277763d392aa23d5cb5009ee91c9bc0ec
Tomer Yehezkely <twik10@gmail.com>
2024-07-03 21:28:26 +0300

    New version complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPage from './AdminPage';
 import EditVolunteerAreas from './EditVolunteerAreas';
 import Registration from './Registration';
+import AdminPage2 from './AdminPage2';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<AdminPage />} />
-                <Route path="/edit-volunteer-areas" element={<EditVolunteerAreas />} />
+                {/* <Route path="/" element={<AdminPage />} />
+                <Route path="/edit-volunteer-areas" element={<EditVolunteerAreas />} /> */}
 
                 {/* <Route path="/" element={<Registration />} /> */}
+                <Route path="/" element={<AdminPage2 />} />
 
             </Routes>
         </Router>
