1333af8aeeeb132633c3cddd31425b6c75fafb1e
Tomer Yehezkely <twik10@gmail.com>
2024-07-13 18:57:15 +0300

    final version

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
 import Registration from './Registration';
 import AdminPage2 from './AdminPage2';
-import Login from './Login';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                {/* <Route path="/Registration" element={<Registration />} /> */}
-                <Route path="/" element={<AdminPage2 />} />
-                {/* <Route path="/login" element={<Login />} /> */}
-                {/* <Route path="/AdminPage2" element={<AdminPage2 />} /> */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<Registration />} />
+        {/* <Route path="/AdminPage2" element={<AdminPage2 />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- testBL.py
+++ testBL.py
@@ -125,4 +125,4 @@ def approve_volunteer():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
